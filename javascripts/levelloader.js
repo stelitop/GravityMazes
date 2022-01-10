04b33aaf700d5066e0d6dab33e88d8de752a4041
stelitop <31509581+stelitop@users.noreply.github.com>
2022-01-10 20:04:19 +0100

    load levels synchroniously

--- javascripts/levelloader.js
+++ javascripts/levelloader.js
@@ -4,23 +4,45 @@ $.getJSON("levels/levelList.json", function(result) {
     for (let i = 0; i < result.urls.length; i++) {
         //alert(result.urls[i]);
 
-        $.getJSON("levels/" + result.urls[i], function(levelObj) {
-            //alert(JSON.stringify(levelObj));
-            let lvl = Level.fromObject(levelObj);
-            levelList.push(lvl);
-
-            let button = document.createElement("div");
-
-            button.className = "levelbutton";
-            button.innerText = lvl.name + "\n" + lvl.superlvl + "-" + lvl.sublvl;
-            const curLvl = levelList.length - 1;
-            button.onclick = function() {
-                curGame = Level.fromObject(JSON.parse(JSON.stringify(levelList[curLvl])))
-                curGame.draw();
+        $.ajax({
+            url: "levels/" + result.urls[i],
+            dataType: 'json',
+            async: false,
+            success: function(levelObj) {
+                //alert(JSON.stringify(levelObj));
+                let lvl = Level.fromObject(levelObj);
+                levelList.push(lvl);
+    
+                let button = document.createElement("div");
+    
+                button.className = "levelbutton";
+                button.innerText = lvl.name + "\n" + lvl.superlvl + "-" + lvl.sublvl;
+                const curLvl = levelList.length - 1;
+                button.onclick = function() {
+                    curGame = Level.fromObject(JSON.parse(JSON.stringify(levelList[curLvl])))
+                    curGame.draw();
+                }
+    
+                document.body.appendChild(button);
             }
+        })
+        // $.getJSON("levels/" + result.urls[i], function(levelObj) {
+        //     //alert(JSON.stringify(levelObj));
+        //     let lvl = Level.fromObject(levelObj);
+        //     levelList.push(lvl);
 
-            document.body.appendChild(button);
-        });
+        //     let button = document.createElement("div");
+
+        //     button.className = "levelbutton";
+        //     button.innerText = lvl.name + "\n" + lvl.superlvl + "-" + lvl.sublvl;
+        //     const curLvl = levelList.length - 1;
+        //     button.onclick = function() {
+        //         curGame = Level.fromObject(JSON.parse(JSON.stringify(levelList[curLvl])))
+        //         curGame.draw();
+        //     }
+
+        //     document.body.appendChild(button);
+        // });
     }
 })
 
